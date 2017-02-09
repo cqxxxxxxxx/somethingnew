95cc5f3d8480ea2ad5a9fd46c0e8df79ded3efdf
Shan <ss>
2017-02-09 18:23:59 +0800

    1.add some BigDecimal test. 2.add some JAVA8 time api demo

--- stn-java8/src/main/java/com/cqx/time/LocalDateDemo.java
+++ stn-java8/src/main/java/com/cqx/time/LocalDateDemo.java
@@ -1,16 +1,109 @@
 package com.cqx.time;
 import org.junit.Test;
 
+import javax.xml.crypto.Data;
 import java.time.*;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.Temporal;
 import java.time.temporal.TemporalAdjusters;
 import java.time.temporal.TemporalField;
+import java.util.Calendar;
+import java.util.Date;
 
 /**
- * java8的新的时间API
+ * java8的新的时间API {@link LocalDateTime}
+ * 更多的使用 参考 @see http://www.jianshu.com/p/191f1f5ee82b
+ *
  * Created by Shan on 2017/2/8.
  */
 public class LocalDateDemo {
 
+    /**
+     *
+     * 一些基础的操作
+     * {@link LocalDate}        日期
+     * {@link LocalTime}        时间
+     * {@link LocalDateTime}    日期+时间
+     * {@link Instant}
+     */
+    @Test
+    public void basicTest(){
+        LocalDate date = LocalDate.now();
+        System.out.println(date);       //2017-02-09
+
+        LocalTime time = LocalTime.now();
+        System.out.println(time);       //18:10:36.493
+
+        LocalDateTime dateTime = LocalDateTime.now();
+        System.out.println(dateTime);   //2017-02-09T18:10:36.493
+
+        Instant instant = Instant.now();
+        Instant instant1 = dateTime.atZone(ZoneId.systemDefault()).toInstant(); //LocalDateTime转Instan
+        System.out.println(instant);                //2017-02-09T10:18:16.849Z
+        System.out.println(instant.toEpochMilli()); //转为unix timeStamp
+
+
+        //向后兼容 对原有date calendar 添加了一些新方法使之能转为新的时间API
+        Instant newDate1, newDate2;
+        Date oldDate = new Date();
+        newDate1 = oldDate.toInstant();  // date -> instant
+        Calendar oldCalendar = Calendar.getInstance();
+        newDate2 = oldCalendar.toInstant();      // calendar -> instant
+        System.out.println(newDate1 + "_____" + newDate1);
+
+    }
+
+
+    /**
+     * {@link java.time.temporal.TemporalAdjuster} 是个函数式接口
+     * {@link TemporalAdjusters} 则提供了他的一些些实现    用于获取firstDayOfMonth之类的东西
+     *
+     * 日期的一些操作
+     * 1.获取某天的开始结束时间
+     * 2.获取某月的开始结束时间
+     */
+    @Test
+    public void dateManipulation(){
+        LocalDate today = LocalDate.now();
+        LocalDateTime dateTime = LocalDateTime.now();
+
+        //获取一天的开始与结尾
+        LocalDateTime startOfToday = today.atStartOfDay();
+        LocalDateTime endOfToday = today.atTime(LocalTime.MAX);
+
+        //获取一个月的开始于结尾
+        LocalDateTime firstDayOfMonth = dateTime.with(TemporalAdjusters.firstDayOfMonth());   //获取当前月的第一天
+        LocalDateTime lastDayOfMonth = dateTime.with(TemporalAdjusters.lastDayOfMonth());     //获取当前月的最后一天
+
+        //一些常量
+        System.out.println(LocalTime.MAX);  //23:59:59.999999999
+        System.out.println(LocalTime.MIN);  //00:00
+
+
+        //加减运算
+        dateTime.plus(2, ChronoUnit.WEEKS);    //加上2星期
+        dateTime.minus(2, ChronoUnit.DAYS);    //减去2天
+        dateTime.minusHours(2l);   //减去2小时
+        dateTime.plusYears(1l);    //加上1年
+
+
+    }
+
+
+    /**
+     * {@link Period}   处理日期相关的时间段
+     * {@link Duration} 处理时间相关的时间段
+     */
+    @Test
+    public void periodAndDuration(){
+
+
+
+    }
+
+    /**
+     * 杂乱无章的一些东西
+     */
     @Test
     public void localDateTest(){
 
@@ -45,14 +138,15 @@ public class LocalDateDemo {
 
 
         //创建Instant
+        Date date = new Date();
+        Instant instant1 = date.toInstant();    //java8支持的向后转型，提供给date转化为Instant的方法
+
         Instant instant = Instant.now();
         System.out.println(instant.toString());
         System.out.println(instant.toEpochMilli()); //unix时间戳 纳秒级别的
 
         //LocalDateTime 与 unix时间戳转换  -> 先转为 instant 然后获取long
         long unixTime = LocalDateTime.now().atZone(ZoneId.systemDefault()).toInstant().toEpochMilli();
-
-
 
 
     }
---
+++ stn-test/src/main/java/com/cqx/common/BigDecimalBoom.java
@@ -0,0 +1,26 @@
+package com.cqx.common;
+
+import org.testng.annotations.Test;
+
+import java.math.BigDecimal;
+
+/**
+ * Created by Shan on 2017/2/9.
+ */
+public class BigDecimalBoom {
+
+    /**
+     * BigDecimal的加减乘除都是运算好后返回一个新的BigDecimal 原来的对象值不变
+     */
+    @Test
+    public void caculateTest(){
+        BigDecimal param1 = BigDecimal.ZERO;
+        BigDecimal result = param1.add(BigDecimal.TEN);
+        System.out.println(result);
+
+
+        BigDecimal param2 = BigDecimal.ZERO;
+        param2 = param2.add(BigDecimal.ONE);
+        System.out.println(param2);
+    }
+}
