9f8959da00b3a8b1e12418fabd5abd120fd66bac
cqxxxxxxxx <cqxxxxxxxx@gmail.com>
2017-03-13 22:54:40 +0800

    add scheduled task demo

---
+++ stn-scheduled/src/main/java/com/cqx/spring/AppConfig.java
@@ -0,0 +1,15 @@
+package com.cqx.spring;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * Created by admin on 2017/3/13.
+ */
+@Configuration
+@EnableAsync
+@EnableScheduling
+public class AppConfig {
+
+}
