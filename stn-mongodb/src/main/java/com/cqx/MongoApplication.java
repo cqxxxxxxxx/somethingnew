7b9c2018fb9aeee39df72517bdd054ef38daf20e
BG307435 <jxchen35@best-inc.com>
2018-02-09 19:06:03 +0800

    0.0

--- stn-mongodb/src/main/java/com/cqx/MongoApplication.java
+++ stn-mongodb/src/main/java/com/cqx/MongoApplication.java
@@ -1,13 +1,13 @@
 package com.cqx;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
  * Created by BG307435 on 2017/8/4.
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class MongoApplication {
 
     public static void main(String[] args) {
