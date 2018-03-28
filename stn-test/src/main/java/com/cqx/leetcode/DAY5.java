37e4e253680c8e3ba09804fb183d19083997f90f
BG307435 <jxchen35@best-inc.com>
2018-03-28 13:33:54 +0800

    DAY5

---
+++ stn-test/src/main/java/com/cqx/leetcode/DAY5.java
@@ -0,0 +1,52 @@
+package com.cqx.leetcode;
+
+/**
+ * Created by BG307435 on 2018/3/28.
+ */
+public class DAY5 {
+
+    /**
+     * Input:
+     * Tree 1                     Tree 2
+     * 1                         2
+     * / \                       / \
+     * 3   2                     1   3
+     * /                           \   \
+     * 5                             4   7
+     * Output:
+     * Merged tree:
+     * 3
+     * / \
+     * 4   5
+     * / \   \
+     * 5   4   7
+     *
+     * @param t1
+     * @param t2
+     * @return
+     */
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if (t1 == null && t2 == null) return null;
+
+        int val = (t1 == null ? 0 : t1.val) + (t2 == null ? 0 : t2.val);
+        TreeNode newNode = new TreeNode(val);
+
+        newNode.left = mergeTrees(t1 == null ? null : t1.left, t2 == null ? null : t2.left);
+        newNode.right = mergeTrees(t1 == null ? null : t1.right, t2 == null ? null : t2.right);
+
+        return newNode;
+
+    }
+
+    //Definition for a binary tree node.
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+}
