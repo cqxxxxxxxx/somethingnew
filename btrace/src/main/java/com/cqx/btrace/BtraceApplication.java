b6908e51a88f6bbbd6655306099539245441afbc
cqx <82803852@qq.com>
2018-09-12 00:06:07 +0800

    1. btrace demo

---
+++ btrace/src/main/java/com/cqx/btrace/BtraceApplication.java
@@ -0,0 +1,12 @@
+package com.cqx.btrace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtraceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BtraceApplication.class, args);
+    }
+}
