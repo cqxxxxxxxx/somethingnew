7dc8c846c6f4cd611778a50dce448ffbabbd9cc6
BG307435 <jxchen35@best-inc.com>
2018-11-13 15:52:55 +0800

    1. jvm related 2. excel to xml(sax解析)

---
+++ stn-tools/src/main/java/com/cqx/poi/ExcelToXml.java
@@ -0,0 +1,131 @@
+package com.cqx.poi;
+
+import org.apache.poi.openxml4j.opc.OPCPackage;
+import org.apache.poi.xssf.eventusermodel.XSSFReader;
+import org.apache.poi.xssf.model.SharedStringsTable;
+import org.apache.poi.xssf.usermodel.XSSFRichTextString;
+import org.xml.sax.*;
+import org.xml.sax.helpers.DefaultHandler;
+import org.xml.sax.helpers.XMLReaderFactory;
+
+import java.io.InputStream;
+import java.util.Iterator;
+
+/**
+ * @author BG307435
+ * @date 2018/9/29
+ */
+public class ExcelToXml {
+
+    public static void main(String[] args) throws Exception {
+        ExcelToXml example = new ExcelToXml();
+        example.processOneSheet("C:\\Users\\BG307435\\Desktop\\关账sql\\应付导入模版.xlsx");
+        example.processAllSheets("C:\\Users\\BG307435\\Desktop\\关账sql\\应付导入模版.xlsx");
+    }
+
+    public void processOneSheet(String filename) throws Exception {
+        OPCPackage pkg = OPCPackage.open(filename);
+        XSSFReader r = new XSSFReader(pkg);
+        SharedStringsTable sst = r.getSharedStringsTable();
+
+        XMLReader parser = fetchSheetParser(sst);
+
+        // To look up the Sheet Name / Sheet Order / rID,
+        //  you need to process the core Workbook stream.
+        // Normally it's of the form rId# or rSheet#
+        InputStream sheet2 = r.getSheet("rId2");
+        InputSource sheetSource = new InputSource(sheet2);
+        parser.parse(sheetSource);
+
+//        InputStream inputStream = r.getSheetsData().next();
+//        byte[] a = IOUtils.toByteArray(inputStream);
+//        File file = new File("C:\\Users\\BG307435\\Desktop\\关账sql\\adfasfa.xml");
+//        FileOutputStream fileOutputStream = new FileOutputStream(file);
+//        fileOutputStream.write(a);
+        sheet2.close();
+    }
+
+    public void processAllSheets(String filename) throws Exception {
+        OPCPackage pkg = OPCPackage.open(filename);
+        XSSFReader r = new XSSFReader(pkg);
+        SharedStringsTable sst = r.getSharedStringsTable();
+
+        XMLReader parser = fetchSheetParser(sst);
+
+        Iterator<InputStream> sheets = r.getSheetsData();
+        while (sheets.hasNext()) {
+            System.out.println("Processing new sheet:\n");
+            InputStream sheet = sheets.next();
+            InputSource sheetSource = new InputSource(sheet);
+            parser.parse(sheetSource);
+            sheet.close();
+            System.out.println("");
+        }
+    }
+
+    public XMLReader fetchSheetParser(SharedStringsTable sst) throws SAXException {
+        XMLReader parser =
+                XMLReaderFactory.createXMLReader(
+                        "org.apache.xerces.parsers.SAXParser"
+                );
+        ContentHandler handler = new SheetHandler(sst);
+        parser.setContentHandler(handler);
+        return parser;
+    }
+
+    /**
+     * See org.xml.sax.helpers.DefaultHandler javadocs
+     */
+    private static class SheetHandler extends DefaultHandler {
+        private SharedStringsTable sst;
+        private String lastContents;
+        private boolean nextIsString;
+
+        private SheetHandler(SharedStringsTable sst) {
+            this.sst = sst;
+        }
+
+        @Override
+        public void startElement(String uri, String localName, String name,
+                                 Attributes attributes) throws SAXException {
+            // c => cell
+            if (name.equals("c")) {
+                // Print the cell reference
+                System.out.print(attributes.getValue("r") + " - ");
+                // Figure out if the value is an index in the SST
+                String cellType = attributes.getValue("t");
+                if (cellType != null && cellType.equals("s")) {
+                    nextIsString = true;
+                } else {
+                    nextIsString = false;
+                }
+            }
+            // Clear contents cache
+            lastContents = "";
+        }
+
+        @Override
+        public void endElement(String uri, String localName, String name)
+                throws SAXException {
+            // Process the last contents as required.
+            // Do now, as characters() may be called more than once
+            if (nextIsString) {
+                int idx = Integer.parseInt(lastContents);
+                lastContents = new XSSFRichTextString(sst.getEntryAt(idx)).toString();
+                nextIsString = false;
+            }
+
+            // v => contents of a cell
+            // Output after we've seen the string contents
+            if (name.equals("v")) {
+                System.out.println(lastContents);
+            }
+        }
+
+        @Override
+        public void characters(char[] ch, int start, int length)
+                throws SAXException {
+            lastContents += new String(ch, start, length);
+        }
+    }
+}
