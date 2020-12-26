a705674a38497ed5ae28997e3e37002c8870b918
cqx <82803852@qq.com>
2020-12-26 21:37:06 +0800

    1. spring cloud gateway

---
+++ stn-scgateway/src/main/java/com/cqx/stn/scgateway/StnScgatewayApplication.java
@@ -0,0 +1,13 @@
+package com.cqx.stn.scgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StnScgatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StnScgatewayApplication.class, args);
+	}
+
+}
